043b78e81827797c472e9b3de36264e035a3e4d2
Curtis Jiang <jqqqqqqqqqq@gmail.com>
2020-04-19 01:24:02 -0400

    fix version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version():
     """
     txt = (WORK_DIR / 'mirai_core' / '__init__.py').read_text('utf-8')
     try:
-        return re.findall(r"^__VERSION__ = (.*)[\r\n]?$", txt, re.M)[0]
+        return re.findall(r"^__VERSION__ = '(.*)'[\r\n]?$", txt, re.M)[0]
     except IndexError:
         raise RuntimeError('Unable to determine version.')
 
